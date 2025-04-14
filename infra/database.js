144f434321f42f621193dae006c1e2b3fef544f4
Lucas Henrique Alves Rosa <65405310+LucasHARosa@users.noreply.github.com>
2025-04-14 21:24:17 +0000

    feat: atualizar configuração de conexão com PostgreSQL para usar novas credenciais e host

--- infra/database.js
+++ infra/database.js
@@ -1,27 +1,25 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
-  await client.connect();
-  try{
+  try {
+    await client.connect();
     const res = await client.query(queryObject);
     return res;
-  }
-  catch (error) {
+  } catch (error) {
     console.error("Database connection error:", error);
     throw error;
   } finally {
     await client.end();
   }
-  
 }
 
 export default {
