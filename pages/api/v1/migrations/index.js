8ccaa689e8eafd093c3b1f6bb3cb38179236f006
Lucas Henrique Alves Rosa <65405310+LucasHARosa@users.noreply.github.com>
2025-04-16 13:35:25 +0000

    feat: implementar endpoint de migrações com retorno de detalhes do banco de dados

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,13 @@
-import database from "infra/database.js";
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
 
 export default async function migrations(req, res) {
-  
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  });
+  res.status(200).json(migrations);
 }
-
-
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,6 @@
 test("GET to /api/v1/migrations return 200", async () => {
   const res = await fetch(`http://localhost:3000/api/v1/migrations`);
   expect(res.status).toBe(200);
-  
+  const responseBody = await res.json();
+  expect(Array.isArray(responseBody)).toBe(true);
 });
-
-
