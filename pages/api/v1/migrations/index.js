b0e48c20c6dcfc20cf01c64a288dd41ebe0d42a4
Lucas Henrique Alves Rosa <65405310+LucasHARosa@users.noreply.github.com>
2025-04-17 18:52:25 +0000

    feat: refatorar lógica de migrações e melhorar testes de integração

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,35 +2,40 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(req, res) {
+  const defaultMigrationOptions = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
   if (req.method !== "GET" && req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
+
   if (!process.env.DATABASE_URL) {
     return res.status(500).json({ error: "DATABASE_URL not set" });
   }
+
   if (req.method === "POST") {
     console.log("Running migrations...");
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-    res.status(200).json(migrations);
+
+    if (migratedMigrations.length > 0) {
+      return res.status(201).json(migratedMigrations);
+    }
+
+    res.status(200).json(migratedMigrations);
   }
 
   if (req.method === "GET") {
     console.log("Fetching migrations...");
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    res.status(200).json(migrations);
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    res.status(200).json(pendingMigrations);
   }
 }
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,17 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public");
+}
+
 test("POST to /api/v1/migrations return 200", async () => {
   const res = await fetch(`http://localhost:3000/api/v1/migrations`, {
     method: "POST",
   });
-  expect(res.status).toBe(200);
+  expect(res.status).toBe(201);
   const responseBody = await res.json();
-  console.log(responseBody);
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
