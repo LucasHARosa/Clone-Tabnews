32072e9a61f7ef796a64278eb3e245e717c65afd
Lucas Henrique Alves Rosa <65405310+LucasHARosa@users.noreply.github.com>
2025-05-29 12:30:38 +0000

    feat: add wait-for-postgress

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const {exec} = require("node:child_process")
+
+function checkPotgres(){
+  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn)
+  function handleReturn(error,stdout){
+    if(stdout.search("accepting connections")=== -1){
+      process.stdout.write(".")
+      checkPotgres()
+      return;
+    }
+    
+    console.log("\nPronto para receber conexões\n")
+    
+  }
+}
+
+ process.stdout.write("\n\nAguardando Postgres aceitar conexões")
+checkPotgres()
