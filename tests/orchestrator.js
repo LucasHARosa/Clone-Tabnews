5b1973c0033fa93a33544bde80566b793de864c2
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2025-06-03 07:38:33 -0300

    fix: corrigir formatação e adicionar consistência no código do orquestrador

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,22 +1,22 @@
-import retry from "async-retry"
+import retry from "async-retry";
 
-async function waitForAllServices(){
+async function waitForAllServices() {
   await waitForWebServer();
 
-  async function waitForWebServer(){
-    return retry(fetchStatusPage,{
-      retries:100,
-      maxTimeout:1000,
-    })
-    async function fetchStatusPage(){
-      const response = await fetch("http://localhost:3000/api/v1/status") 
-      const responseBody = await response.json()
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      if (response.status !== 200) {
+        throw new Error("Status page not ready");
+      }
     }
   }
-
-  
 }
 
-export default{
+export default {
   waitForAllServices,
-}
+};
