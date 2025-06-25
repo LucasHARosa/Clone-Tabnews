b25c7a37fb0979e3b68e701bd25085bcb4f29fe6
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2025-06-25 15:32:53 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,13 +4,17 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status return 200", async () => {
-  const res = await fetch(`http://localhost:3000/api/v1/status`);
-  expect(res.status).toBe(200);
-  const json = await res.json();
-  //console.log(json);
-  expect(json).toHaveProperty("updated_at");
-  expect(json.dependecies.database.version).toEqual("16.0");
-  expect(json.dependecies.database.max_connections).toEqual(100);
-  expect(json.dependecies.database.connections_used).toEqual(1);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const res = await fetch(`http://localhost:3000/api/v1/status`);
+      expect(res.status).toBe(200);
+      const json = await res.json();
+      //console.log(json);
+      expect(json).toHaveProperty("updated_at");
+      expect(json.dependecies.database.version).toEqual("16.0");
+      expect(json.dependecies.database.max_connections).toEqual(100);
+      expect(json.dependecies.database.connections_used).toEqual(1);
+    });
+  });
 });
