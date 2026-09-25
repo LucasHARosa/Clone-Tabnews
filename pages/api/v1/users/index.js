4767a5a5d3e9836bc1e700c04f542f31d7ee42e5
LucasHARosa <l.ucas.henrique.a.r@gmail.com>
2026-09-25 15:54:17 -0300

    feat: implement user creation functionality and update tests

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,5 +1,6 @@
 import controller from "@/infra/controller.js";
 import { createRouter } from "next-connect";
+import user from "@/models/user.js";
 
 const router = createRouter();
 
@@ -8,5 +9,7 @@ router.post(postHandler);
 export default router.handler(controller.errorHandler);
 
 async function postHandler(req, res) {
-  res.status(201).json({});
+  const userInputValues = req.body;
+  const createdUser = await user.create(userInputValues);
+  res.status(201).json(createdUser);
 }
--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
 import orchestrator from "@/tests/orchestrator.js";
-import database from "@/infra/database.js";
+import { version as uuidVersion } from "uuid";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -10,18 +10,32 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
-      await database.query(
-        "INSERT INTO users (username, email, password) VALUES ('testuser', 'testuser@example.com', 'password123')",
-      );
-      await database.query(
-        "INSERT INTO users (username, email, password) VALUES ('testuser2', 'Testuser@example.com', 'password123')",
-      );
-      const users = await database.query("SELECT * FROM users");
-      console.log("users log", users.rows);
       const res = await fetch(`http://localhost:3000/api/v1/users`, {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "testuser",
+          email: "testuser@example.com",
+          password: "password123",
+        }),
       });
+
       expect(res.status).toBe(201);
+      const responseBody = await res.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "testuser",
+        email: "testuser@example.com",
+        password: "password123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
     });
   });
 });
